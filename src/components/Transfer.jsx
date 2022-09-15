084f9aa52279ef02632804ccf7c0d6a6131c1e90
Chambo015 <thegunner125@gmail.com>
2022-09-15 14:18:42 +0600

    add input

--- src/components/Transfer.jsx
+++ src/components/Transfer.jsx
@@ -7,6 +7,11 @@ import {
   FlatList,
   Text,
   Image,
+  TextInput,
+  TouchableHighlight,
+  ScrollView,
+  KeyboardAvoidingView,
+  Keyboard
 } from 'react-native';
 import {
   Entypo,
@@ -31,6 +36,8 @@ export default function Transfer({ navigation }) {
   const refFlatListContacts = useRef(null);
   const [indexCard, setIndexCard] = useState(0);
   const [indexContact, setIndexContact] = useState(null);
+  const [value, setValue] = useState('');
+  const [purposeText, setPurposeText] = useState('');
 
   useEffect(() => {
     refFlatListCard.current?.scrollToIndex({
@@ -39,10 +46,10 @@ export default function Transfer({ navigation }) {
       viewPosition: 0.65,
     });
     refFlatListContacts.current?.scrollToIndex({
-        index: indexContact,
-        animated: true,
-        viewPosition: 0.5,
-      });
+      index: indexContact,
+      animated: true,
+      viewPosition: 0.5,
+    });
   }, [indexCard, indexContact]);
   const renderCards = ({ item, index }) => {
     return (
@@ -83,9 +90,9 @@ export default function Transfer({ navigation }) {
     );
   };
   return (
-    <SafeAreaView style={styles.container}>
+    <KeyboardAvoidingView  style={styles.container}>
       <StatusBar style="auto" />
-      <View style={styles.main}>
+      <ScrollView style={styles.main}>
         <Text style={[styles.textBalanceCard, { marginBottom: 20 }]}>From</Text>
         <View>
           <FlatList
@@ -99,41 +106,88 @@ export default function Transfer({ navigation }) {
           />
         </View>
         <Text style={[styles.textBalanceCard, { marginVertical: 20 }]}>To</Text>
-        <FlatList
-          ref={refFlatListContacts}
-          data={DATA.contacts}
-          contentContainerStyle={{ paddingLeft: CARD_PADDING }}
-          keyExtractor={(item) => item.id}
-          renderItem={({ item, index }) => (
-            <TouchableOpacity
-            onPress={() => setIndexContact(index)}
-              style={{
-                borderTopLeftRadius: 50,
-                borderTopRightRadius: 50,
-                borderBottomLeftRadius: 30,
-                borderBottomRightRadius: 30,
-                marginRight: 15,
-                height: 120,
-                borderWidth: 2,
-                borderColor: index === indexContact ? COLORS.active : COLORS.inactive,
-                justifyContent: 'space-between',
-                paddingBottom: 5
-              }}
-            >
-              <Image
-                style={{ width: 75, height: 75, borderRadius: 50, borderWidth: 2,
-                    borderColor: index === indexContact ? COLORS.active : COLORS.inactive, }}
-                source={{ uri: item.avatar }}
+        <View>
+          <FlatList
+            ref={refFlatListContacts}
+            data={DATA.contacts}
+            ListHeaderComponent={() => (
+              <TouchableOpacity>
+                <AntDesign name="plus" size={40} color="#213569" />
+              </TouchableOpacity>
+            )}
+            ListHeaderComponentStyle={styles.ListHeader}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item, index }) => (
+              <TouchableOpacity
+                onPress={() => setIndexContact(index)}
+                style={[
+                  styles.contactsItem,
+                  {
+                    borderColor:
+                      index === indexContact ? COLORS.active : COLORS.inactive,
+                  },
+                ]}
+              >
+                <Image
+                  style={[
+                    styles.contactsImage,
+                    {
+                      borderColor:
+                        index === indexContact
+                          ? COLORS.active
+                          : COLORS.inactive,
+                    },
+                  ]}
+                  source={{ uri: item.avatar }}
+                />
+                <Text style={styles.contactsName}>{item.name}</Text>
+              </TouchableOpacity>
+            )}
+            horizontal
+            snapToInterval={CARD_WIDTH + CARD_PADDING}
+            showsHorizontalScrollIndicator={false}
+          />
+        </View>
+        <Text style={[styles.textBalanceCard, { marginVertical: 20 }]}>
+          Amount
+        </Text>
+        <View>
+          <NumericFormat
+            value={value}
+            displayType="text"
+            thousandSeparator
+            valueIsNumericString={true}
+            prefix="$"
+            renderText={(value) => (
+              <TextInput
+                style={styles.inputStyle}
+                placeholderTextColor="white"
+                placeholder="$0.00"
+                onChangeText={setValue}
+                value={value}
+                keyboardType="numeric"
               />
-              <Text style={styles.contactsName}>{item.name}</Text>
-            </TouchableOpacity>
-          )}
-          horizontal
-          snapToInterval={CARD_WIDTH + CARD_PADDING}
-          showsHorizontalScrollIndicator={false}
-        />
-      </View>
-    </SafeAreaView>
+            )}
+          />
+        </View>
+        <Text style={[styles.textBalanceCard, { marginVertical: 20 }]}>
+          Purpose
+        </Text>
+        <View style={{marginBottom: 50}}>
+          <TextInput
+            style={styles.inputStyle}
+            placeholderTextColor="#626262"
+            placeholder="Education"
+            onChangeText={setPurposeText}
+            value={purposeText}
+          />
+        </View>
+       
+      </ScrollView>
+      <TouchableHighlight onPress={Keyboard.dismiss} style={styles.submitButton}>
+          <Text style={styles.submitButtonText}>Send</Text>
+        </TouchableHighlight>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -146,6 +200,7 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingHorizontal: 25,
     paddingVertical: 20,
+    marginBottom: 100
   },
   cardWrap: {
     width: CARD_WIDTH,
@@ -173,11 +228,63 @@ const styles = StyleSheet.create({
     fontSize: 20,
     lineHeight: 30,
   },
+  contactsItem: {
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    borderBottomLeftRadius: 30,
+    borderBottomRightRadius: 30,
+    marginRight: 15,
+    height: 120,
+    borderWidth: 2,
+    justifyContent: 'space-between',
+    paddingBottom: 5,
+  },
+  contactsImage: {
+    width: 75,
+    height: 75,
+    borderRadius: 50,
+    borderWidth: 2,
+  },
   contactsName: {
     color: '#F9F9F9',
     fontWeight: '500',
     fontSize: 12,
     lineHeight: 18,
+    textAlign: 'center',
+  },
+  ListHeader: {
+    width: 75,
+    height: 75,
+    backgroundColor: '#DBE3F8',
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginRight: 15,
+  },
+  inputStyle: {
+    color: '#FFFFFF',
+    fontWeight: '500',
+    fontSize: 20,
+    lineHeight: 30,
+    paddingVertical: 5,
+    borderBottomWidth: StyleSheet.hairlineWidth,
+    borderBottomColor: '#626262',
+  },
+  submitButton: {
+    paddingVertical: 15,
+    backgroundColor: '#414A61',
+    borderRadius: 14,
+    position: 'absolute',
+    bottom: 20,
+    flex: 1,
+    left: 25,
+    right: 25
+  },
+  submitButtonText: {
+    color: '#F9F9F9',
+    fontWeight: '500',
+    fontSize: 20,
+    lineHeight: 30,
     textAlign: 'center',
   },
 });
